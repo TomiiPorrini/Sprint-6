6a13b60ae73a35718c1fb469ef67b409dfdd5e0c
TomiiPorrini <tomasjaja15@hotmail.com>
2022-07-31 15:00:44 -0300

    ultimos cambios. Listas problematicas 1 y 2

--- Problematica1/insertTipoCuenta.sql
+++ Problematica1/insertTipoCuenta.sql
@@ -3,9 +3,21 @@ account_type_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 account_type_description TEXT NOT NULL
 )
 
-
-INSERT INTO tipo_cuenta (account_type_id,account_type_description)
+INSERT INTO tipo_cuenta (account_type_description)
 VALUES
-  (1,"Classic"),
-  (2,"Gold"),
-  (3,"Black")
+  ("Caja de ahorro en Pesos"),
+  ("Caja de ahorro en Dolares"),
+  ("Cuenta corriente en Pesos"),
+  ("Cuenta corriente en Dolares"),
+  ("Cuenta inversion")
+
+CREATE TABLE tipo_cliente (
+customer_type_id integer not null PRIMARY KEY AUTOINCREMENT,
+customer_type_description text not null
+)
+
+INSERT INTO tipo_cliente (customer_type_description)
+VALUES
+  ("Classic"),
+  ("Gold"),
+  ("Black")

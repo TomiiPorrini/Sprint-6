13404fb6c64a983ea88f8de6d9f3ff5c9d5c2a68
gianbarbaro <gianlbarbaro@gmail.com>
2022-07-31 22:14:10 -0300

    problematica 3

--- Problematica3/problematica3.sql
+++ Problematica3/problematica3.sql
@@ -1,23 +1,32 @@
+#1
 SELECT * FROM cuenta WHERE balance < 0;
 
+#2
 SELECT customer_name, customer_surname, (strftime('%Y', 'now') - strftime('%Y', dob)) AS age
 FROM cliente WHERE customer_surname LIKE "%z%";
 
+#3
 SELECT customer_name, customer_surname, (strftime('%Y', 'now') - strftime('%Y', dob)) AS age, branch_name 
 FROM cliente, sucursal WHERE customer_name LIKE "brendan" ORDER BY branch_name;
 
+#4
 SELECT loan_total FROM prestamo WHERE loan_total/100 > 80000 
 UNION SELECT loan_type FROM prestamo WHERE loan_type LIKE "prendario";
 
+#5
 SELECT * FROM prestamo WHERE loan_total > (SELECT avg(loan_total) FROM prestamo);
 
+#6
 SELECT count(customer_name) AS menores_de_50 FROM cliente 
 WHERE (strftime('%Y', 'now') - strftime('%Y', dob)) < 50;
 
+#7
 SELECT * FROM cuenta WHERE balance > 8000 LIMIT 5;
 
+#8
 SELECT * FROM prestamo WHERE (strftime('%m', loan_date)) IN ("04", "06", "08")
 ORDER BY loan_total;
 
+#9
 SELECT loan_type, sum(loan_total) AS loan_total_accu FROM prestamo 
 GROUP BY loan_type;
